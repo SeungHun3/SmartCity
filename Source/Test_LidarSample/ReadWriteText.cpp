c88bd0f49cac27a36b97d508a48900eb0a6c412a
DESKTOP-5VI5HM0\USER <devnexy@gmail.com>
2023-07-10 17:27:29 +0900

    [텍스트 파일] 저장 bool 값 대신 숫자로 수정, 형식에 안 맞으면 기본 값으로 초기화

--- Source/Test_LidarSample/ReadWriteText.cpp
+++ Source/Test_LidarSample/ReadWriteText.cpp
@@ -24,7 +24,17 @@ bool UReadWriteText::LoadLocalTxt(FString FileName, TArray<FString>& LoadSoundAr
 			SaveTypeArray[0].ParseIntoArray(LoadSoundArray, TEXT(","), true); //Sound
 			SaveTypeArray[1].ParseIntoArray(LoadMuteArray, TEXT(","), true); //Mute
 		}
-		
+		// 배열 길이 체크, 초기화
+		if (LoadSoundArray.Num() != 4)
+		{
+			LoadSoundArray.Init("5", 4);
+		}
+
+		if (LoadMuteArray.Num() != 4)
+		{
+			LoadMuteArray.Init("0", 4);
+		}
+
 		return true;
 	}
 	else 
@@ -42,6 +52,17 @@ bool UReadWriteText::SaveLocalTxt(TArray<FString> SaveSoundArray, TArray<FString
 	SaveTypeArray.Add(FString::Join(SaveSoundArray, TEXT(","))); //Sound
 	SaveTypeArray.Add(FString::Join(SaveMuteArray, TEXT(","))); //Mute
 	
+	// 배열 길이 체크, 초기화
+	if (SaveSoundArray.Num() != 4)
+	{
+		SaveSoundArray.Init("5", 4);
+	}
+
+	if (SaveMuteArray.Num() != 4)
+	{
+		SaveMuteArray.Init("0", 4);
+	}
+
 	//각 타입은 개행 문자로 Join
 	FString OutPutString = FString::Join(SaveTypeArray, TEXT("\n"));
 
